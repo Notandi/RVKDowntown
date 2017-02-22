312fac48297fdc53331e3b6e110c0e54950bcde1
Óskar Ólafsson <Notandi@users.noreply.github.com>
2017-02-22 16:08:09 +0000

    Bætti við api request meðhöndlun

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var dm = require('../models/datamanager');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/api/ids', function(req, res, next){
+  var ids = dm.getBarIds();
+  res.send("hi");
+});
+
+router.post('/api/bars',function(req, res, next){
+  var idString = req.body.ids;
+  console.log(req.body);
+  var response = dm.getBars( idString );
+  res.send(response);
 });
 
 module.exports = router;
