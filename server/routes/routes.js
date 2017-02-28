ec3545bcaf460a1a499974b4c459476aca23b2e9
Óskar Ólafsson <Notandi@users.noreply.github.com>
2017-02-28 11:52:34 +0000

    bætti við comments í routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -22,12 +22,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+// returns a list of barids
 router.get('/api/ids', function(req, res, next){
   dataManager.getBarIds( (barids)=>{
     res.send(barids);
   });
 });
 
+/**
+* Returns the bars related to the barids
+*
+* @param {int[]} req.body  - json object with the ids requested
+*/
 router.post('/api/bars',function(req, res, next){
   var ids = req.body;
   dataManager.getBars( ids, (bars) => {
